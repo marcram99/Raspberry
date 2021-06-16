b64fa36b67e7f33e054e271627be86e80949f441
marcram99 <marcram99@gmail.com>
2021-06-16 17:29:20 +0200

    work on relibility

--- light_logger.py
+++ light_logger.py
@@ -1,40 +1,56 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path    
 
 date_stamp = f'{datetime.now():%Y-%m}'
 logfiles_path = Path.home().joinpath('Marc-perso/Code/logfiles')
-
 if not logfiles_path.exists():
     Path.mkdir(logfiles_path)
-logfile_name = f'{date_stamp}_capt02.log'    
-logfile = logfiles_path.joinpath(logfile_name)
+logfile = logfiles_path.joinpath(f'{date_stamp}_capt02.log')
+if not logfile.exists():
+    with open(logfile, 'w') as f:
+        f.write('\n')
+datafile = Path.cwd().joinpath('data.txt')
+time_mailwarning = timedelta(hours=1)
 
-capt_value = 'dark'
-
-with open('data.txt') as json_file:
+with open(datafile) as json_file:
     data = json.load(json_file)
+    stored_lux = data['light_mode']
+    stored_time = data['time_stamp']
+print(25*'-')
+print(f"dernière valeur lue: {stored_lux} @ {stored_time}")
+print(25*'-')
+
+capt_value = input('valeur du capeur de lum:')
+
 if data['light_mode'] == 'dark':
     if capt_value == 'dark':
         pass
-    else:
+    if capt_value == 'light':
         time_stamp = datetime.now()
         data['light_mode'] = 'light'
         data['time_stamp'] = f'{time_stamp:%Y-%m-%d %H:%M:%S}'
-        log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to {data['light_mode']}\n"
+        log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to light\n"
         with open(logfile,'a') as f:
             f.write(log_data)
         with open ('data.txt', 'w') as outfile:
             json.dump(data,outfile)
 if data['light_mode'] == 'light':
+    first_light = datetime.strptime(data['time_stamp'],'%Y-%m-%d %H:%M:%S')
+    time_stamp = datetime.now()
+    time_diff = time_stamp - first_light
     if capt_value == 'dark':
-         time_stamp = datetime.now()
-         data['light_mode'] = 'dark'
-         data['time_stamp'] = f'{time_stamp:%Y-%m-%d %H:%M:%S}'
-         log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to {data['light_mode']}\n"
-         with open(logfile,'a') as f:
-             f.write(log_data)
-         with open ('data.txt', 'w') as outfile:
-             json.dump(data,outfile)
-
-print(f"{data['light_mode']}")
+        print(f'la lumière a été allumée pendant: {time_diff}')
+        data['light_mode'] = 'dark'
+        data['time_stamp'] = f'{time_stamp:%Y-%m-%d %H:%M:%S}'
+        log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to dark\n"
+        with open(logfile,'a') as f:
+            f.write(log_data)
+        with open ('data.txt', 'w') as outfile:
+            json.dump(data,outfile)
+    if capt_value == 'light':
+        print(f' lumière est allumée depuis: {time_diff}')
+        if time_diff > time_mailwarning:
+            log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_mail envoyé pour alarme +1h.\n"
+            print("lumière allumée depuis plus d'une heure: un mail à été envoyé!")
+            print(f"finish prog with:{data['light_mode']}")
--- temp_logger.py
+++ temp_logger.py
@@ -23,3 +23,4 @@ hum = sensor.relative_humidity
 data = f'{time_stamp}_T:{temp:.2f}_H:{hum:.1f}\n'
 with open(logfile,'a') as f:
     f.write(data)
+
