62e99e3ca7727e6e990c94208fbd76df95ef0d73
marcram99 <marcram99@gmail.com>
2021-06-17 17:50:35 +0200

    working correctely v1

--- light_logger.py
+++ light_logger.py
@@ -4,53 +4,74 @@ from pathlib import Path
 
 date_stamp = f'{datetime.now():%Y-%m}'
 logfiles_path = Path.home().joinpath('Marc-perso/Code/logfiles')
+logfile = logfiles_path.joinpath(f'{date_stamp}_capt02.log')
 if not logfiles_path.exists():
     Path.mkdir(logfiles_path)
-logfile = logfiles_path.joinpath(f'{date_stamp}_capt02.log')
 if not logfile.exists():
-    with open(logfile, 'w') as f:
-        f.write('\n')
-datafile = Path.cwd().joinpath('data.txt')
+    logfile.touch()
+datafile = Path.cwd().joinpath('data.json')
+if not datafile.exists():
+    with open(datafile,'w')as new_file:
+        json.dump({'light_mode':'dark','change_time':'','mail_time': '', 'mail_alert':0},new_file)
 time_mailwarning = timedelta(hours=1)
 
 with open(datafile) as json_file:
     data = json.load(json_file)
     stored_lux = data['light_mode']
-    stored_time = data['time_stamp']
+    stored_time = data['change_time']
+    mail_alert = data['mail_alert']
+    mail_time = data['mail_time']
 print(25*'-')
 print(f"dernière valeur lue: {stored_lux} @ {stored_time}")
-print(25*'-')
-
 capt_value = input('valeur du capeur de lum:')
 
-if data['light_mode'] == 'dark':
+if stored_lux == 'dark':
     if capt_value == 'dark':
         pass
     if capt_value == 'light':
         time_stamp = datetime.now()
-        data['light_mode'] = 'light'
-        data['time_stamp'] = f'{time_stamp:%Y-%m-%d %H:%M:%S}'
-        log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to light\n"
         with open(logfile,'a') as f:
-            f.write(log_data)
-        with open ('data.txt', 'w') as outfile:
-            json.dump(data,outfile)
-if data['light_mode'] == 'light':
-    first_light = datetime.strptime(data['time_stamp'],'%Y-%m-%d %H:%M:%S')
+           f.write(f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to light\n")
+        data = {'light_mode':'light',
+                'change_time': f'{time_stamp:%Y-%m-%d %H:%M:%S}',
+                'mail_alert': 0}
+        with open (datafile, 'w') as json_file:
+            json.dump(data, json_file)
+
+if stored_lux == 'light':
     time_stamp = datetime.now()
+    first_light = datetime.strptime(stored_time, '%Y-%m-%d %H:%M:%S')
     time_diff = time_stamp - first_light
     if capt_value == 'dark':
-        print(f'la lumière a été allumée pendant: {time_diff}')
-        data['light_mode'] = 'dark'
-        data['time_stamp'] = f'{time_stamp:%Y-%m-%d %H:%M:%S}'
-        log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to dark\n"
         with open(logfile,'a') as f:
-            f.write(log_data)
-        with open ('data.txt', 'w') as outfile:
-            json.dump(data,outfile)
+           f.write(f"{time_stamp:%Y-%m-%d %H:%M:%S}_room passed to dark\n")
+        data = {'light_mode':'dark',
+                'change_time': f'{time_stamp:%Y-%m-%d %H:%M:%S}',
+                'mail_alert': 0}
+        with open (datafile, 'w') as json_file:
+            json.dump(data,json_file)
+        print(f'la lumière a été allumée pendant: {time_diff}')
     if capt_value == 'light':
-        print(f' lumière est allumée depuis: {time_diff}')
-        if time_diff > time_mailwarning:
-            log_data = f"{time_stamp:%Y-%m-%d %H:%M:%S}_mail envoyé pour alarme +1h.\n"
-            print("lumière allumée depuis plus d'une heure: un mail à été envoyé!")
-            print(f"finish prog with:{data['light_mode']}")
+        if (time_diff > time_mailwarning) :
+            if not mail_alert:
+                with open(logfile, 'a') as f:
+                    f.write(f"{time_stamp:%Y-%m-%d %H:%M:%S}_mail envoyé pour alarme +1h.\n")
+                data = {'light_mode':'light',
+                        'change_time': stored_time, 
+                        'mail_time':f'{time_stamp:%Y-%m-%d %H:%M:%S}',
+                        'mail_alert': 1}
+                with open (datafile, 'w') as json_file:
+                    json.dump(data, json_file)
+                print("lumière allumée depuis plus d'une heure: un mail à été envoyé!")
+            if mail_alert:
+                last_mail = datetime.strptime(mail_time, '%Y-%m-%d %H:%M:%S')
+                time_diff = time_stamp - last_mail
+                if time_diff > time_mailwarning:
+                    with open(logfile, 'a') as f:
+                        f.write(f"{time_stamp:%Y-%m-%d %H:%M:%S}_ nouveau mail envoyé pour alarme +1h.\n")
+                    data = {'light_mode':'light',
+                            'change_time': stored_time, 
+                            'mail_time':f'{time_stamp:%Y-%m-%d %H:%M:%S}',
+                            'mail_alert': 1}
+                    with open (datafile, 'w') as json_file:
+                        json.dump(data, json_file)
