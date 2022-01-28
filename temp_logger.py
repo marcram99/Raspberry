a61584af1ed097c2c2e31948d8482bab76473e5d
marcram99 <marcram99@gmail.com>
2022-01-28 22:45:24 +0100

    add templogger run status to debug logfile

--- temp_logger.py
+++ temp_logger.py
@@ -6,7 +6,16 @@ from datetime import datetime
 from config import Config
 
 time_stamp = f'{datetime.now():%Y-%m-%d %H:%M:%S}'
+
+debug_logfile = Config.debug_logfile
+if not debug_logfile.exists():
+    debug_logfile.touch()
 logfile = Config.temp_logfile
+if not logfile.exists():
+    logfile.touch()
+with open(debug_logfile, 'a') as f:
+    f.write(f"{time_stamp} templogger en cours\n")
+
 temp = capteurs.read_temp()
 hum = capteurs.read_hum()
 data = f'{time_stamp}_T:{temp:.2f}_H:{hum:.1f}\n'
